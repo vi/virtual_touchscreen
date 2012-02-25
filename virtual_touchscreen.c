9e5c11c86a99d1d22569fe659a4875caa6389abf
Vitaly _Vi Shukela <vi0oss@gmail.com>
2012-02-25 03:17:57 +0300

    Stub of the virtual touchscreen

--- virtual_touchscreen.c
+++ virtual_touchscreen.c
@@ -7,13 +7,29 @@
 
 #define MODNAME "virtual_touchscreen"
 
-#define ABS_X_MIN	40
-#define ABS_X_MAX	950
-#define ABS_Y_MIN	80
-#define ABS_Y_MAX	910
+#define ABS_X_MIN	0
+#define ABS_X_MAX	1024
+#define ABS_Y_MIN	0
+#define ABS_Y_MAX	1024
 
-#define	PHDR	0xa400012e
-#define SCPDR	0xa4000136
+#define DEVICE_NAME "virtual_touchscreen"
+static int device_open(struct inode *, struct file *);
+static int device_release(struct inode *, struct file *);
+static ssize_t device_read(struct file *, char *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+
+static int Major;            /* Major number assigned to our device driver */
+static int Device_Open = 0;  /* Is device open?  Used to prevent multiple access to the device */
+
+struct class * cl;
+struct device * dev; 
+
+struct file_operations fops = {
+       read: device_read,
+       write: device_write,
+       open: device_open,
+       release: device_release
+};			
 
 static void do_softint(struct work_struct *work);
 
@@ -60,15 +76,63 @@ static int __init virt_ts_init(void)
 	if (err)
 		goto fail1;
 
+
+    /* Above is evdev part. Below is character device part */
+
+    Major = register_chrdev(0, DEVICE_NAME, &fops);	
+    if (Major < 0) {
+	printk ("Registering the character device failed with %d\n", Major);
+	    goto fail1;
+    }
+    printk ("devsysrq: Major=%d\n", Major);
+
+    cl = class_create(THIS_MODULE, DEVICE_NAME);
+    if (!IS_ERR(cl)) {
+	    dev = device_create(cl, NULL, MKDEV(Major,0), NULL, DEVICE_NAME);
+    }
+
+
 	return 0;
 
  fail1:	input_free_device(virt_ts_dev);
 	return err;
 }
 
+static int device_open(struct inode *inode, struct file *file) {
+    if (Device_Open) return -EBUSY;
+    ++Device_Open;
+    return 0;
+}
+	
+static int device_release(struct inode *inode, struct file *file) {
+    --Device_Open;
+    return 0;
+}
+	
+static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_t *offset) {
+    printk ("<2>Reading from virtual_touchscreen device is meaningless\n");
+    return 0;
+}
+	
+static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t *off) {
+    input_report_key(virt_ts_dev, BTN_TOUCH, 1);
+    input_report_abs(virt_ts_dev, ABS_X, 0x33);
+    input_report_abs(virt_ts_dev, ABS_Y, 0x44);
+	input_sync(virt_ts_dev);
+    input_report_key(virt_ts_dev, BTN_TOUCH, 0);
+	input_sync(virt_ts_dev);
+    return len;
+}
+
 static void __exit virt_ts_exit(void)
 {
 	input_unregister_device(virt_ts_dev);
+
+    if (!IS_ERR(cl)) {
+	    device_destroy(cl, MKDEV(Major,0));
+	    class_destroy(cl);
+    }
+    unregister_chrdev(Major, DEVICE_NAME);
 }
 
 module_init(virt_ts_init);
